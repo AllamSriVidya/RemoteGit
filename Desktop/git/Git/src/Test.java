cd487d7920ef3ddcccbd387a3c779aa1b1bfc3fa
Todd <todd@example.com>
2021-05-05 23:00:05 +0530

    Hii created a file

---
+++ Desktop/git/Git/src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int a = 2;
+		String s = "hii";
+		System.out.println(s + a);
+
+	}
+
+}
